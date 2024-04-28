929017879e5b48051ff1d7ba814a1862d370b04e
Elyes <elyes.h77@gmail.com>
2024-04-28 23:06:16 +0200

    Ajout raccourci (alt = qui souligne la premiere lettre du menu) Ajout d'un item disable, ajout d'un message dans la console quand on clique sur un item

--- src/main/java/com/example/demo1/MainVrai.java
+++ src/main/java/com/example/demo1/MainVrai.java
@@ -21,28 +21,56 @@ public class MainVrai extends Application{
     window.setTitle("Menu zelda");
 
     //cree un menu en haut a gauche
-    Menu fileMenu = new Menu("Configuration de la Partie");
-    Menu fileMenu2 = new Menu("Paramètres");
-    Menu fileMenu3 = new Menu("Aide");
+    Menu partie = new Menu("_Configuration de la Partie");
+    Menu parametres = new Menu("_Paramètres");
+    Menu aide = new Menu("_Aide");
 
     // le contenu du menu quand on le fait defiler
-    fileMenu.getItems().add(new MenuItem("Nouvelle partie... "));
-    fileMenu.getItems().add(new MenuItem("Sauvegarder la partie... "));
-    fileMenu.getItems().add(new MenuItem("Quitter la partie..."));
+
+        Menu nvpartieMenu = new Menu("Nouvelle partie...");
+        nvpartieMenu.setOnAction(e -> System.out.println("Creation d'une nouvelle partie..."));
+        partie.getItems().add(nvpartieMenu);
+
+        Menu sauvegarder = new Menu("Sauvegarder la partie...");
+        sauvegarder.setOnAction(e -> System.out.println("Sauvegarder la partie..."));
+        partie.getItems().add(sauvegarder);
+
+    Menu ouvrir = new Menu("Ouvrir une partie...");
+        ouvrir.setOnAction(e -> System.out.println("Ouvrir une partie..."));
+        partie.getItems().add(ouvrir);
+
+    partie.getItems().add(new SeparatorMenuItem());
+
+        Menu quitter = new Menu("Quitter...");
+        quitter.setOnAction(e -> System.out.println("Quitter la partie..."));
+        partie.getItems().add(quitter);
+    //partie.getItems().add(new MenuItem("Quitter..."));
+
+        Menu configuration = new Menu("Configuration des touches...");
+        configuration.setOnAction(e -> System.out.println("Configuration des touches..."));
+        partie.getItems().add(configuration);
+    //parametres.getItems().add(new MenuItem("Configuration des touches..."));
 
 
-    fileMenu2.getItems().add(new MenuItem("Configuration des touches..."));
-    fileMenu2.getItems().add(new MenuItem("Configuration de la qualité graphique..."));
-    fileMenu2.getItems().add(new MenuItem("Ajustement du Volume..."));
+    parametres.getItems().add(new SeparatorMenuItem());
+    parametres.getItems().add(new MenuItem("Qualité graphique..."));
+    parametres.getItems().add(new MenuItem("Volume..."));
+    parametres.getItems().add(new MenuItem("Luminosité..."));
 
-    fileMenu3.getItems().add(new MenuItem("Nous contacter..."));
-    fileMenu3.getItems().add(new MenuItem("Dictionnaire du jeu..."));
+    aide.getItems().add(new MenuItem("Nous contacter..."));
+    aide.getItems().add(new SeparatorMenuItem());
+    aide.getItems().add(new MenuItem("Dictionnaire du jeu..."));
+
+        Menu adresse = new Menu("Notre adresse...");
+        adresse.setOnAction(e -> System.out.println("T'aura pas l'adresse batard..."));
+        adresse.setDisable(true);
+        aide.getItems().add(adresse);
 
 
     //creation de la barre du menu
     MenuBar menuBar = new MenuBar();
 
-    menuBar.getMenus().addAll(fileMenu, fileMenu2, fileMenu3);
+    menuBar.getMenus().addAll(partie, parametres, aide);
 
 
     layout = new BorderPane();
